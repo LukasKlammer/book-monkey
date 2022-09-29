1c421a0c74b34f61ea54f4d05a1e302c82b5c877
unknown <klammer.lukas@hotmail.com>
2022-09-29 14:17:23 +0200

    convert book form to reactive forms add input fields dynamically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { BookFormComponent } from './book-form/book-form.component';
 import { CreateBookComponent } from './create-book/create-book.component';
 import { FormMessagesComponent } from './form-messages/form-messages.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { FormMessagesComponent } from './form-messages/form-messages.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Book } from '../shared/book';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Book, Thumbnail } from '../shared/book';
 import { BookFactory } from '../shared/book-factory';
 
 @Component({
@@ -9,20 +9,63 @@ import { BookFactory } from '../shared/book-factory';
   styleUrls: ['./book-form.component.scss']
 })
 export class BookFormComponent implements OnInit {
-  book = BookFactory.empty();
+  bookForm: FormGroup;
 
+  @Input() book?: Book;
   @Output() submitBook = new EventEmitter<Book>();
-  @ViewChild('bookForm', { static: true}) form?: NgForm;
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      subtitle: [''],
+      isbn: ['', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13)
+      ]],
+      description: [''],
+      authors: this.buildAuthorsArray(['']),
+      thumbnails: this.buildThumbNailsArray([
+        { title: '', url: '' }
+      ]),
+      published: [new Date(), [Validators.required]]
+    });
+
+  }
 
   ngOnInit(): void {
   }
 
   public submitForm() {
-    this.submitBook.emit(this.book);
-    this.book = BookFactory.empty();
-    this.form?.reset();
+
   }
 
+  private buildAuthorsArray(values: string[]): FormArray {
+    return this.fb.array(values, Validators.required)
+  }
+
+  private buildThumbNailsArray(values: Thumbnail[]): FormArray {
+    return this.fb.array(
+      values.map(t => this.fb.group(t))
+    )
+  }
+
+  get authors(): FormArray {
+    return this.bookForm?.get('authors') as FormArray;
+  }
+
+  get thumbnails(): FormArray {
+    return this.bookForm?.get('thumbnails') as FormArray;
+  }
+
+  addAuthorControl() {
+    this.authors.push(this.fb.control(''));
+  }
+
+  addThumbnailControl() {
+    this.thumbnails.push(
+      this.fb.group( { url: '', title: '' })
+    );
+  }
 }
